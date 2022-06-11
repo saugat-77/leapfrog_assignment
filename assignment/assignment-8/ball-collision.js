710a7d7c543e11be0d46293d3e81681d210f9884
saugat-77 <saugataharya789@gmail.com>
2022-06-11 10:09:01 +0545

    collision left

---
+++ assignment/assignment-8/ball-collision.js
@@ -0,0 +1,102 @@
+const container = document.getElementById("container");
+
+class Ball {
+  constructor(px, py, h, w, speedX, speedY, dx = 1, dy = 1) {
+    this.px = px;
+    this.py = py;
+    this.dx = dx;
+    this.dy = dy;
+    this.h = h;
+    this.w = w;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+
+  createBall() {
+    // console.log("create ball");
+    this.element = document.createElement("div");
+    this.element.style.position = "absolute";
+    this.element.style.height = `${this.h}px`;
+    this.element.style.width = `${this.w}px`;
+    this.element.style.backgroundColor = "red";
+    this.element.style.borderRadius = "50%";
+    this.element.style.top = `${this.px}px`;
+    this.element.style.left = `${this.py}px`;
+    container.appendChild(this.element);
+  }
+  toPx(value) {
+    return `${value}px`;
+  }
+
+  moveBall() {
+    // console.log("move ball");
+
+    this.px += this.speedX * this.dx;  //if called inside class elements use this.feature only
+    this.py += this.speedY * this.dy;
+
+    this.element.style.top = this.toPx(this.px);//if called css, this.csskonaam.style
+    this.element.style.left = this.toPx(this.py);
+
+    // console.log(this.px, this.py);
+  }
+
+  boundaryCheck() {
+    console.log("boundary Check");
+    if (this.px >= 500 - this.h) {
+      this.dx = -1;
+    }
+    if (this.py >= 500 - this.w) {
+      this.dy = -1;
+    }
+    if (this.px <= 0) {
+      this.dx = 1;
+    }
+    if (this.py <= 0) {
+      this.dy = 1;
+    }
+  }
+}
+const containerHeight = 500;
+const containerWidth = 500;
+
+function generateRandomIntegerInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const noOfBalls=10
+let x=[]
+for(let j=0; j<noOfBalls; j++){ //if ball increases incresase here
+  z="ball"+[j+1]
+  x.push(z)
+}
+console.log(x)
+// let x = ["ball1", "ball2", "ball3", "ball4", "ball5"];
+for (let i = 0; i < noOfBalls; i++) { //if ball increases incresase here
+  var px = Math.floor(Math.random() * 450 + 1); //1-50
+  var py = Math.floor(Math.random() * 400 + 1); //1-400
+  var h = Math.floor(Math.random() * 40 + 10);
+  var w = h;
+  var speedX = Math.floor(Math.random() * 7 + 1);
+  var speedY = Math.floor(Math.random() * 7 + 1);
+
+  x[i] = new Ball(px, py, h, w, speedX, speedY);
+  console.log(x[i])
+  x[i].createBall();
+  x[i].moveBall();
+  x[i].boundaryCheck();
+}
+
+
+function play() {
+  for (let i = 0; i < noOfBalls; i++) { //if ball increases incresase here
+
+    x[i].moveBall();
+    x[i].boundaryCheck();
+  }
+
+  {
+    window.requestAnimationFrame(() => {
+      play();
+    });
+  }
+}
+play();
